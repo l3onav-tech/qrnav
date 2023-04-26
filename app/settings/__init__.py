5fb61cef3ab38d3aac84dab72fe25221217a7a0b
Leonardo Nava <L3oNav@outlook.com>
2023-04-25 18:01:22 -0700

    postgres added

--- app/settings/__init__.py
+++ app/settings/__init__.py
@@ -1,2 +1,10 @@
 from .settings import *
+from .settings import Settings
 from .redis import *
+from functools import lru_cache
+
+
+@lru_cache
+def get_settings() -> Settings:
+    settings = Settings()
+    return settings
